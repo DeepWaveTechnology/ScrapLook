0a2c3fa819bf8beb50849fd3b4d3619ed53d10cd
Etsu7 <mateo.massinond@gmail.com>
2025-07-01 21:41:34 +0200

    Init commit

--- scraplook-backend/src/scraplook-backend/prisma/client.py
+++ scraplook-backend/src/scraplook-backend/prisma/client.py
@@ -85,10 +85,10 @@ __all__ = (
 
 log: logging.Logger = logging.getLogger(__name__)
 
-SCHEMA_PATH = Path('C:/Users/AntoninDurand/Documents/ScrapLook/scraplook-backend/schema.prisma')
+SCHEMA_PATH = Path('C:/Users/matvm/Documents/web_projet_de_fou/ScrapLook/scraplook-backend/schema.prisma')
 PACKAGED_SCHEMA_PATH = Path(__file__).parent.joinpath('schema.prisma')
 ENGINE_TYPE: EngineType = EngineType.binary
-BINARY_PATHS = model_parse(BinaryPaths, {'queryEngine': {'windows': 'C:\\Users\\AntoninDurand\\.cache\\prisma-python\\binaries\\5.17.0\\393aa359c9ad4a4bb28630fb5613f9c281cde053\\node_modules\\prisma\\query-engine-windows.exe'}, 'introspectionEngine': {}, 'migrationEngine': {}, 'libqueryEngine': {}, 'prismaFmt': {}})
+BINARY_PATHS = model_parse(BinaryPaths, {'queryEngine': {'windows': 'C:\\Users\\matvm\\.cache\\prisma-python\\binaries\\5.17.0\\393aa359c9ad4a4bb28630fb5613f9c281cde053\\node_modules\\prisma\\query-engine-windows.exe'}, 'introspectionEngine': {}, 'migrationEngine': {}, 'libqueryEngine': {}, 'prismaFmt': {}})
 
 
 class Prisma(AsyncBasePrisma):
@@ -148,7 +148,7 @@ class Prisma(AsyncBasePrisma):
         return {
             'name': 'db',
             'url': OptionalValueFromEnvVar(**{'value': 'file:./dev.db', 'fromEnvVar': None}).resolve(),
-            'source_file_path': 'C:/Users/AntoninDurand/Documents/ScrapLook/scraplook-backend/schema.prisma',
+            'source_file_path': 'C:/Users/matvm/Documents/web_projet_de_fou/ScrapLook/scraplook-backend/schema.prisma',
         }
 
     async def execute_raw(self, query: LiteralString, *args: Any) -> int:
